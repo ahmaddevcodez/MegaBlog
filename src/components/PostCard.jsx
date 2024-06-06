643a8752b49f1c780913f80c45ead466e0598d7d
Muhammad Ahmad Tahir <132302808+ahmaddevcodez@users.noreply.github.com>
2024-06-07 04:00:39 +0500

    image fix

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -26,12 +26,12 @@ function PostCard({ $id, title, featuredImage, content }) {
         <FollowerPointerCard
           title={<TitleComponent title={title} featuredImage={featuredImage} />}
         >
-          <div className="relative overflow-hidden h-full rounded-2xl transition duration-200 group bg-white hover:shadow-xl border border-zinc-100">
+          <div className="overflow-hidden h-full rounded-2xl transition duration-200 group bg-white hover:shadow-xl border border-zinc-100">
             <div className="w-full aspect-w-16 aspect-h-10 bg-gray-100 rounded-tr-lg rounded-tl-lg overflow-hidden xl:aspect-w-16 xl:aspect-h-10 relative">
               <img
                 src={appWriteService.getFilePreview(featuredImage)}
                 alt={title}
-                className="w-full   group-hover:scale-95 group-hover:rounded-2xl transform object-cover transition duration-200 object-contain"
+                className="w-[400px] h-[270px] group-hover:scale-95 group-hover:rounded-2xl transform object-center transition duration-200 "
               />
             </div>
             <div className="p-4">
