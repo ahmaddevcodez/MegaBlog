7f6a5d39f701be24c5b7e797cb81ea8325d8b397
Muhammad Ahmad Tahir <132302808+ahmaddevcodez@users.noreply.github.com>
2024-06-07 03:23:11 +0500

    All post images issue fix and divs

--- src/components/pages/post.jsx
+++ src/components/pages/post.jsx
@@ -33,13 +33,13 @@ export default function Post() {
   };
 
   return post ? (
-    <div className="py-8 bg-black text-white">
+    <div className="py-8 bg-black text-white grid lg:grid-col-3 md:grid-col-2 gap-10 grid-col-1 ">
       <Container>
-        <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
+        <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2 ">
           <img
             src={appWriteService.getFilePreview(post.featuredImage)}
             alt={post.title}
-            className="rounded-xl"
+            className="rounded-xl height-[60%]"
           />
 
           {isAuthor && (
